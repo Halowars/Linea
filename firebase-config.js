266f42086e1b3f777eb87a5df2def9e7374e13c7
Tristan Means <89054000+Halowars@users.noreply.github.com>
2026-06-04 15:25:01 -0400

    Add Linea Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.LINEA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD48Mo1r2-2H6wz6W1nNyFRLgzUTQyy2w8",
+  authDomain: "linea-e80b7.firebaseapp.com",
+  projectId: "linea-e80b7",
+  storageBucket: "linea-e80b7.firebasestorage.app",
+  messagingSenderId: "387254938699",
+  appId: "1:387254938699:web:fdd81a56b8f5e20172b1ce",
+  measurementId: "G-1PY5DPGM2N",
+};
